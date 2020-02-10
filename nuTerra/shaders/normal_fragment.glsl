406b4ddf9e9d8e7d99c190ccd4177359ed8f0bbd
Simi4 <maxim.like.python@ya.ru>
2020-02-10 17:10:44 +0300

    Small geom shader cleanup

--- nuTerra/shaders/normal_fragment.glsl
+++ nuTerra/shaders/normal_fragment.glsl
@@ -1,4 +1,4 @@
-﻿#version 430 compatibility
+﻿#version 430 core
 
 layout (location = 0) out vec4 gColor;
 
--- nuTerra/shaders/normal_geo.glsl
+++ nuTerra/shaders/normal_geo.glsl
@@ -1,26 +1,27 @@
-﻿#version 430 compatibility
+﻿#version 430 core
+#extension GL_EXT_geometry_shader4 : enable
 
 layout (triangles) in;
-layout (line_strip) out;
-layout (max_vertices = 18) out;
-#extension GL_EXT_geometry_shader4 : enable
-//
+layout (line_strip, max_vertices = 18) out;
+
 uniform int mode;
 uniform float prj_length;
 uniform mat4 MVPM;
 
- in vec3 n[3];
- in vec3 t[3];
- in vec3 b[3];
- out vec4 color;
+in vec3 n[3];
+in vec3 t[3];
+in vec3 b[3];
+out vec4 color;
+
 void main()
 {
     vec4 sumV;
     vec4 sumN;
     if (mode == 1) {
+        sumV = (gl_PositionIn[0] + gl_PositionIn[1] + gl_PositionIn[2]) / 3.0;
+
         //Normal
             color = vec4(1.0,0.0,0.0,1.0);
-            sumV = (gl_PositionIn[0] + gl_PositionIn[1] + gl_PositionIn[2]) / 3.0;
             sumN.xyz = (n[0].xyz + n[1].xyz + n[2].xyz) / 3.0;
             sumN.w = 0.0;
             gl_Position = MVPM * sumV;
@@ -30,7 +31,6 @@ void main()
             EndPrimitive();
         //Tangent
             color = vec4(0.0,1.0,0.0,1.0);
-            sumV = (gl_PositionIn[0] + gl_PositionIn[1] + gl_PositionIn[2]) / 3.0;
             sumN.xyz = (t[0].xyz + t[1].xyz + t[2].xyz) / 3.0;
             sumN.w = 0.0;
             gl_Position = MVPM * sumV;
@@ -40,7 +40,6 @@ void main()
             EndPrimitive();
         //biTangent
             color = vec4(0.0,0.0,1.0,1.0);
-            sumV = (gl_PositionIn[0] + gl_PositionIn[1] + gl_PositionIn[2]) / 3.0;
             sumN.xyz = (b[0].xyz + b[1].xyz + b[2].xyz) / 3.0;
             sumN.w = 0.0;
             gl_Position = MVPM * sumV;
