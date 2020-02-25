b14c1517f5b9c6c66ebd2329daae25d3ab48cda4
mike <thecooltool@hotmail.com>
2020-02-25 14:20:35 -0500

    compacted code for seam generation

--- nuTerra/MapLoader/ChunkFunctions.vb
+++ nuTerra/MapLoader/ChunkFunctions.vb
@@ -571,7 +571,6 @@ Module ChunkFunctions
         Dim x_pos As Integer = 0
         Dim yu, yl, xu, xl As Single
         Dim tl, bl, tr, br, cur_x, cur_y As Single
-        Dim tl_n, tr_n, bl_n, br_n As UInt32
         Dim v_cnt As Integer = 0
         Dim buff(2) As vertex_data
         'Dim debug_string As String = ""
@@ -601,53 +600,42 @@ Module ChunkFunctions
                     End If
                     Valid_data = True
                     u_start = 0
-                    'top Seam
+
+                    'HORIZONTAL SEAM
                     For x1 = mapBoard(mbX, mbY).location.X - 50 To _
                                                 mapBoard(mbX, mbY).location.X + 50 - (scale * 2) Step 1 * scale
-                        'pack_2_10_10_10(v_data.normals((i), (j + 1)), 0)
-                        tl = theMap.v_data(mapBoard(mbX, mbY + 1).map_id).heights(x_pos, 0)
-                        tl_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY + 1).map_id).normals(x_pos, 0),
-                                               theMap.v_data(mapBoard(mbX, mbY + 1).map_id).holes(x_pos, 0))
-
-                        tr = theMap.v_data(mapBoard(mbX, mbY + 1).map_id).heights(x_pos + 1, 0)
-                        tr_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY + 1).map_id).normals(x_pos + 1, 0),
-                                               theMap.v_data(mapBoard(mbX, mbY + 1).map_id).holes(x_pos + 1, 0))
-
-                        bl = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(x_pos, 63)
-                        bl_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(x_pos, 63),
-                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(x_pos, 63))
-
-                        br = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(x_pos + 1, 63)
-                        br_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(x_pos + 1, 63),
-                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(x_pos + 1, 63))
 
                         theMap.v_data(mapBoard(mbX, mbY).map_id).heights(x_pos, 64) = tl
 
                         topleft.vert.X = x1
                         topleft.vert.Y = yu
-                        topleft.H = tl
-                        topleft.norm = tl_n
+                        topleft.H = theMap.v_data(mapBoard(mbX, mbY + 1).map_id).heights(x_pos, 0)
+                        topleft.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY + 1).map_id).normals(x_pos, 0),
+                                               theMap.v_data(mapBoard(mbX, mbY + 1).map_id).holes(x_pos, 0))
                         topleft.uv.X = u_start
                         topleft.uv.Y = almost1
 
                         bottomleft.vert.X = x1
                         bottomleft.vert.Y = yl
-                        bottomleft.H = bl
-                        bottomleft.norm = bl_n
+                        bottomleft.H = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(x_pos, 63)
+                        bottomleft.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(x_pos, 63),
+                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(x_pos, 63))
                         bottomleft.uv.X = u_start
                         bottomleft.uv.Y = almost1 - uvinc
 
                         topRight.vert.X = x1 + scale
                         topRight.vert.Y = yu
-                        topRight.H = tr
-                        topRight.norm = tr_n
+                        topRight.H = theMap.v_data(mapBoard(mbX, mbY + 1).map_id).heights(x_pos + 1, 0)
+                        topRight.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY + 1).map_id).normals(x_pos + 1, 0),
+                                               theMap.v_data(mapBoard(mbX, mbY + 1).map_id).holes(x_pos + 1, 0))
                         topRight.uv.X = u_start + uvinc
                         topRight.uv.Y = almost1
 
                         bottomRight.vert.X = x1 + scale
                         bottomRight.vert.Y = yl
-                        bottomRight.H = br
-                        bottomRight.norm = br_n
+                        bottomRight.H = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(x_pos + 1, 63)
+                        bottomRight.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(x_pos + 1, 63),
+                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(x_pos + 1, 63))
                         bottomRight.uv.X = u_start + uvinc
                         bottomRight.uv.Y = almost1 - uvinc
 
@@ -667,57 +655,46 @@ Module ChunkFunctions
                     If Not mapBoard(mbX + 1, mbY).occupied Then
                         GoTo endx
                     End If
-
                     Valid_data = True
 
-                    'Corner
-                    'this part does the one corner we can't loop in to
-                    tl = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, 63)
-                    tl_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(63, 63),
-                                           theMap.v_data(mapBoard(mbX, mbY).map_id).holes(63, 63))
-
-                    tr = theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, 63)
-                    tr_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY).map_id).normals(0, 63),
-                                           theMap.v_data(mapBoard(mbX + 1, mbY).map_id).holes(0, 63))
-
-                    bl = theMap.v_data(mapBoard(mbX, mbY + 1).map_id).heights(63, 0)
-                    bl_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY + 1).map_id).normals(63, 0),
-                                           theMap.v_data(mapBoard(mbX, mbY + 1).map_id).holes(63, 0))
-
-                    br = theMap.v_data(mapBoard(mbX + 1, mbY + 1).map_id).heights(0, 0)
-                    br_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY + 1).map_id).normals(0, 0),
-                                           theMap.v_data(mapBoard(mbX + 1, mbY + 1).map_id).holes(0, 0))
-
+                    'CORNER
                     'these 3 positions was a pain to sort out :)
-                    theMap.v_data(mapBoard(mbX, mbY).map_id).heights(64, 64) = br 'ok
-                    theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, 64) = bl 'ok
-                    theMap.v_data(mapBoard(mbX, mbY).map_id).heights(64, 63) = tr 'ok
+                    theMap.v_data(mapBoard(mbX, mbY).map_id).heights(64, 64) =
+                        theMap.v_data(mapBoard(mbX + 1, mbY + 1).map_id).heights(0, 0) 'ok
+                    theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, 64) =
+                        theMap.v_data(mapBoard(mbX, mbY + 1).map_id).heights(63, 0) 'ok
+                    theMap.v_data(mapBoard(mbX, mbY).map_id).heights(64, 63) =
+                        theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, 63) 'ok
 
                     topleft.vert.X = cur_x + scale
                     topleft.vert.Y = yl
-                    topleft.H = tl
-                    topleft.norm = tl_n
+                    topleft.H = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, 63)
+                    topleft.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(63, 63),
+                                           theMap.v_data(mapBoard(mbX, mbY).map_id).holes(63, 63))
                     topleft.uv.X = almost1 - uvinc
                     topleft.uv.Y = almost1 - uvinc
 
                     topRight.vert.X = cur_x + (scale * 2)
                     topRight.vert.Y = yl
-                    topRight.H = tr
-                    topRight.norm = tr_n
+                    topRight.H = theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, 63)
+                    topRight.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY).map_id).normals(0, 63),
+                                           theMap.v_data(mapBoard(mbX + 1, mbY).map_id).holes(0, 63))
                     topRight.uv.X = almost1
                     topRight.uv.Y = almost1 - uvinc
 
                     bottomleft.vert.X = cur_x + scale
                     bottomleft.vert.Y = yu
-                    bottomleft.H = bl
-                    bottomleft.norm = bl_n
+                    bottomleft.H = theMap.v_data(mapBoard(mbX, mbY + 1).map_id).heights(63, 0)
+                    bottomleft.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY + 1).map_id).normals(63, 0),
+                                           theMap.v_data(mapBoard(mbX, mbY + 1).map_id).holes(63, 0))
                     bottomleft.uv.X = almost1 - uvinc
                     bottomleft.uv.Y = almost1
 
                     bottomRight.vert.X = cur_x + (scale * 2)
                     bottomRight.vert.Y = yu
-                    bottomRight.H = br
-                    bottomRight.norm = br_n
+                    bottomRight.H = theMap.v_data(mapBoard(mbX + 1, mbY + 1).map_id).heights(0, 0)
+                    bottomRight.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY + 1).map_id).normals(0, 0),
+                                           theMap.v_data(mapBoard(mbX + 1, mbY + 1).map_id).holes(0, 0))
                     bottomRight.uv.X = almost1
                     bottomRight.uv.Y = almost1
 
@@ -729,9 +706,7 @@ Module ChunkFunctions
                     buff(v_cnt) = bottomleft : v_cnt += 1
                     buff(v_cnt) = bottomRight : v_cnt += 1
                     buff(v_cnt) = topRight : v_cnt += 1
-
 endx:
-
                     If Not mapBoard(mbX + 1, mbY).occupied Then
                         GoTo endy
                     End If
@@ -743,53 +718,42 @@ endx:
                     cur_y = 0
                     y_pos = 0
                     v_start = 0
-                    'right seam
+                    'VERTICAL SEAM
                     For y1 = mapBoard(mbX, mbY).location.Y - 50 To _
                               mapBoard(mbX, mbY).location.Y + 50 - (scale * 2) Step 1 * scale
 
-                        tl = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, y_pos + 1)
-                        tl_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(63, y_pos + 1),
-                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(63, y_pos + 1))
-
-                        tr = theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, y_pos + 1)
-                        tr_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY).map_id).normals(0, y_pos + 1),
-                                               theMap.v_data(mapBoard(mbX + 1, mbY).map_id).holes(0, y_pos + 1))
-
-                        bl = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, y_pos)
-                        bl_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(63, y_pos),
-                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(63, y_pos))
-
-                        br = theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, y_pos)
-                        br_n = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY).map_id).normals(0, y_pos),
-                                               theMap.v_data(mapBoard(mbX + 1, mbY).map_id).holes(0, y_pos))
-
-                        theMap.v_data(mapBoard(mbX, mbY).map_id).heights(64, y_pos) = br
+                        theMap.v_data(mapBoard(mbX, mbY).map_id).heights(64, y_pos) =
+                                theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, y_pos + 1)
 
                         topleft.vert.X = xl
                         topleft.vert.Y = y1 + scale
-                        topleft.H = tl
-                        topleft.norm = tl_n
+                        topleft.H = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, y_pos + 1)
+                        topleft.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(63, y_pos + 1),
+                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(63, y_pos + 1))
                         topleft.uv.X = almost1 - uvinc
                         topleft.uv.Y = v_start + uvinc
 
                         bottomleft.vert.X = xl
                         bottomleft.vert.Y = y1
-                        bottomleft.H = bl
-                        bottomleft.norm = bl_n
+                        bottomleft.H = theMap.v_data(mapBoard(mbX, mbY).map_id).heights(63, y_pos)
+                        bottomleft.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX, mbY).map_id).normals(63, y_pos),
+                                               theMap.v_data(mapBoard(mbX, mbY).map_id).holes(63, y_pos))
                         bottomleft.uv.X = almost1 - uvinc
                         bottomleft.uv.Y = v_start
 
                         topRight.vert.X = xu
                         topRight.vert.Y = y1 + scale
-                        topRight.H = tr
-                        topRight.norm = tr_n
+                        topRight.H = theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, y_pos + 1)
+                        topRight.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY).map_id).normals(0, y_pos + 1),
+                                               theMap.v_data(mapBoard(mbX + 1, mbY).map_id).holes(0, y_pos + 1))
                         topRight.uv.X = almost1
                         topRight.uv.Y = v_start + uvinc
 
                         bottomRight.vert.X = xu
                         bottomRight.vert.Y = y1
-                        bottomRight.H = br
-                        bottomRight.norm = br_n
+                        bottomRight.H = theMap.v_data(mapBoard(mbX + 1, mbY).map_id).heights(0, y_pos)
+                        bottomRight.norm = pack_2_10_10_10(theMap.v_data(mapBoard(mbX + 1, mbY).map_id).normals(0, y_pos),
+                                               theMap.v_data(mapBoard(mbX + 1, mbY).map_id).holes(0, y_pos))
                         bottomRight.uv.X = almost1
                         bottomRight.uv.Y = v_start
 
