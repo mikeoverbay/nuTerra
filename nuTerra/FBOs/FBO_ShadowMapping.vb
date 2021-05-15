be6f069c513306dd7d7c6d070ca163e618b4e47b
Simi4 <maxim.like.python@ya.ru>
2021-05-15 11:22:06 +0300

    Init ShadowMappingFBO

---
+++ nuTerra/FBOs/FBO_ShadowMapping.vb
@@ -0,0 +1,40 @@
+﻿Imports OpenTK.Graphics.OpenGL4
+
+Public Class ShadowMappingFBO
+    Public Shared fbo As GLFramebuffer
+    Public Shared depth_tex As GLTexture
+
+    Public Const WIDTH = 1024
+    Public Const HEIGHT = 1024
+
+    Public Shared Sub FBO_Initialize()
+        frmMain.glControl_main.MakeCurrent()
+
+        create_textures()
+
+        If Not create_fbo() Then
+            MsgBox("Failed to create ShadowMapping FBO" + vbCrLf + "I must shut down!", MsgBoxStyle.Exclamation, "We're Screwed!")
+            End
+        End If
+    End Sub
+
+    Public Shared Sub create_textures()
+        depth_tex = GLTexture.Create(TextureTarget.Texture2D, "depth_tex")
+        depth_tex.Parameter(TextureParameterName.TextureMinFilter, TextureMinFilter.Linear)
+        depth_tex.Parameter(TextureParameterName.TextureMagFilter, TextureMagFilter.Linear)
+        'depth_tex.Parameter(TextureParameterName.TextureCompareMode, TextureCompareMode.CompareRefToTexture)
+        'depth_tex.Parameter(TextureParameterName.TextureCompareFunc, DepthFunction.Lequal)
+        depth_tex.Storage2D(1, DirectCast(PixelInternalFormat.DepthComponent32f, SizedInternalFormat), WIDTH, HEIGHT)
+    End Sub
+
+    Public Shared Function create_fbo() As Boolean
+        fbo = GLFramebuffer.Create("ShadowMappingFBO")
+        fbo.Texture(FramebufferAttachment.DepthAttachment, depth_tex, 0)
+
+        If Not fbo.IsComplete Then
+            Return False
+        End If
+
+        Return True ' No errors! all is good! :)
+    End Function
+End Class
