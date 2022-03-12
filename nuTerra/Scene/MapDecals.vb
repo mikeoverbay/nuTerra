b12142afbce1acf8f4e37e84ac5417f83291acfd
Simi4 <maxim.like.python@ya.ru>
2022-03-12 15:10:33 +0300

    Fix compilation

--- nuTerra/Scene/MapDecals.vb
+++ nuTerra/Scene/MapDecals.vb
@@ -1,8 +1,15 @@
 ﻿
 
 Public Class MapDecals
-    Public Sub New()
+    Implements IDisposable
 
+    ReadOnly scene As MapScene
+
+    Public Sub New(scene As MapScene)
+        Me.scene = scene
     End Sub
 
+    Public Sub Dispose() Implements IDisposable.Dispose
+
+    End Sub
 End Class
